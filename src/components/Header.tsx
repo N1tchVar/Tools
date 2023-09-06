65c12951585d091217479a6fecee39ed22d8fa84
Iraklu <chaxva32@gmail.com>
2023-09-05 23:38:19 -0700

    updates :fire:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
-
+import { IconBulb } from '@tabler/icons-react'
+import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="fixed inset-x-0 top-0 z-40 bg-black/80 shadow-sm saturate-100 backdrop-blur-[10px]">
+        <div className="mx-auto flex h-[60px] max-w-5xl items-center justify-between px-8">
+            <Link href="/"><IconBulb/></Link>
+        </div>
+    </header>
   )
 }
 
