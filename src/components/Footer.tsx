3610bc5867d412d9cc9b3cdba0085c956c0ebfae
Iraklu <chaxva32@gmail.com>
2023-09-17 02:44:32 -0700

    bug fixing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <div className="mx-auto flex max-w-4xl flex-col pb-8">
+    <div className="mx-auto flex max-w-4xl px-8 flex-col pb-8">
       © 2023 N1tch
     </div>
   )
